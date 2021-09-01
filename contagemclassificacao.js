7d5a385dc13cbe68814554136db09744d76e214c
Adriano L C Soares <adrian.louis.003@live.com>
2021-09-01 11:57:46 -0300

    Atividade de contagem e classificação em JS

---
+++ contagemclassificacao.js
@@ -0,0 +1,15 @@
+let alunos = 0;
+
+while (alunos <= 10) {
+    
+
+    if ( alunos === 0){
+        console.log(alunos + " É o número ZERO");
+    } else if (alunos % 2 == 0)
+    console.log(alunos + " É número par");
+    else {
+        console.log(alunos + " É número ímpar");
+    }
+    alunos ++;
+    
+}
